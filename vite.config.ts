83b724eaf7ded8f975170062d8b6653d5c141778
Mark Liang <mark@vdr.tw>
2025-05-08 14:31:35 +0800

    feat:  build docs

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
